97ab15589b2ee0ff46841ab113f2891551c984db
Stephen Fulljames <stephen@fulljames.net>
2015-09-30 15:15:25 +0100

    Basic polling on centres model

--- assets/js/views/centres.js
+++ assets/js/views/centres.js
@@ -13,11 +13,18 @@ module.exports = Backbone.View.extend({
   template: _.template(templates.centre),
 
   initialize: function() {
+    var $this = this;
     this.model.on('change',this.render, this);
     this.model.fetch();
+
+    setInterval(function(){
+      $this.model.fetch();
+    },3000);
   },
 
   render: function() {
+    this.$el.empty();
+
     _.each(this.model.attributes, function(item) {
       item.booked_and_reserved = item.booked + item.reserved;
       this.$el.append(this.template(item));
