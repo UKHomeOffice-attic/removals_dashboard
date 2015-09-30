f55b2ecc8f3daff708af80ebf7f0ed086ce5aa46
Stephen Fulljames <stephen@fulljames.net>
2015-09-30 14:18:59 +0100

    Remove runnable app code

--- routes/centres.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var router = express.Router();
-var appRoutes = require('./appRoutes');
-
-/* GET centres listing. */
-router.get('/', appRoutes['/centres'].fn);
-
-module.exports = router;
--- routes/index.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var router = express.Router();
-var appRoutes = require('./appRoutes');
-
-/* GET home page. */
-router.get('/', appRoutes['/'].fn);
-
-module.exports = router;
