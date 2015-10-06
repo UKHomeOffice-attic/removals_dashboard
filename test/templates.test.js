8a4e99e44ff62c266ab74002b62c8be42d548620
Stephen Fulljames <stephen@fulljames.net>
2015-10-06 16:34:59 +0100

    Fix failing template test after introducing bed partial

--- test/templates.test.js
+++ test/templates.test.js
@@ -7,7 +7,19 @@ describe('templates', function() {
   describe('centres', function() {
     it('should render the centre template correctly with data', function() {
 
-      var output = templates.centre.render({name: 'Heathrow',male_available: 0, female_available: 0, booked_and_reserved: 0});
+      var output = templates.centre.render({
+        name: 'Heathrow',
+        beds: [{
+          type: "male",
+          available: 0
+        },{
+          type: "female",
+          available: 0
+        }],
+        booked_and_reserved: 0
+      },{
+        bed: templates.bed
+      });
 
       expect(output).to.contain('<h2>Heathrow</h2>');
       expect(output).to.contain('<span class=\'bed-count-figure\'>0</span> <span class=\'bed-count-label\'>male beds available</span>');
