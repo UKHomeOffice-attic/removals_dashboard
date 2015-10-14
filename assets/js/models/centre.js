f48c736c47247faa9cd324a025207bd4f660dbd9
Stephen Fulljames <stephen@fulljames.net>
2015-10-14 11:21:24 +0100

    Test centre model population via socket

--- assets/js/models/centre.js
+++ assets/js/models/centre.js
@@ -3,7 +3,11 @@ var Backbone = require('backbone');
 module.exports = Backbone.Model.extend({
   urlRoot: 'centres.json',
   defaults: {
-    "bedcount": 0
+    name: undefined,
+    centre_id: undefined,
+    beds: [],
+    booked: 0,
+    reserved: 0
   },
 
   initialize: function(attributes, options) {
@@ -13,8 +17,8 @@ module.exports = Backbone.Model.extend({
     if (options && options.socket) this.socket = options.socket;
 
     if (this.socket) {
-      this.socket.on('bedcount', function(payload) {
-        $this.set('bedcount', payload.beds);
+      this.socket.on('populate', function(payload) {
+        $this.set(payload);
       });
     }
 
--- test/models.test.js
+++ test/models.test.js
@@ -53,6 +53,7 @@ describe('collections', function () {
 describe('models', function() {
   describe('centre', function() {
     beforeEach(function() {
+      var io;
 
       this.server = new socket();
       this.fakeio = function(server) {
@@ -61,19 +62,25 @@ describe('models', function() {
         }
       };
 
+      io = sailsIOClient(this.fakeio(this.server));
+      io.sails.autoConnect = false;
+
+      this.socket0 = io.sails.connect();
     });
     it('should be instantiated', function() {
       var model = new models.Centre();
 
       expect(model).not.to.be(undefined);
+      expect(model.get('name')).to.be(undefined);
+      expect(model.get('centre_id')).to.be(undefined);
+      expect(model.get('beds')).to.eql([]);
+      expect(model.get('booked')).to.be(0);
+      expect(model.get('reserved')).to.be(0);
+
     });
+
     it('should respond to socket events', function(done) {
-      var io = sailsIOClient(this.fakeio(this.server));
-
-      io.sails.autoConnect = false;
-
-      var socket0 = io.sails.connect();
-      var model = new models.Centre([], { socket: socket0 });
+      var model = new models.Centre([], { socket: this.socket0 });
 
       model.socket.on('message', function(payload) {
         expect(payload).to.be('test message');
@@ -83,21 +90,51 @@ describe('models', function() {
       this.server.emit('message','test message');
     });
 
-    it('should update its bed count on a relevant socket message', function() {
-      var io = sailsIOClient(this.fakeio(this.server));
+    it('should populate its data on initial payload', function(done) {
+      var model = new models.Centre([], { socket: this.socket0 });
 
-      io.sails.autoConnect = false;
+      model.on('change', function() {
+        expect(model.get('name')).to.be('Heathrow');
+        expect(model.get('centre_id')).to.be(1);
+        expect(model.get('beds').length).to.be(2);
+        expect(model.get('booked')).to.be(20);
+        expect(model.get('reserved')).to.be(20);
 
-      var socket0 = io.sails.connect();
-      var model = new models.Centre([], { socket: socket0 });
-
-      expect(model.get('bedcount')).to.be(0);
-
-      model.on('change:bedcount', function() {
-        expect(model.get('bedcount')).to.be(1);
+        expect(model.get('beds')[0].type).to.be("male");
+        done();
       });
 
-      this.server.emit('bedcount', { beds: 1 });
+      this.server.emit('populate', {
+        name: "Heathrow",
+        centre_id: 1,
+        beds: [{
+          type: "male",
+          available: 35,
+          ooc: 3
+        },{
+          type: "female",
+          available: 53,
+          ooc: 2
+        }],
+        booked: 20,
+        reserved: 20
+      });
+
+    });
+
+    xit('should update its data on subsequent payloads', function(done) {
+      var model = new models.Centre([], { socket: this.socket0 });
+
+      model.set('booked',20);
+
+      model.on('change:booked', function() {
+        expect(model.get('booked')).to.be(21);
+        done();
+      });
+
+      this.server.emit('update', {
+        booked: 21
+      });
 
     });
   });
