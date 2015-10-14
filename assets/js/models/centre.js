8ef1938117fc74f67a61eb82229f96415c8bc24f
Stephen Fulljames <stephen@fulljames.net>
2015-10-14 10:29:36 +0100

    Initial set up of websockets

--- assets/js/models/centre.js
+++ assets/js/models/centre.js
@@ -1,5 +1,22 @@
 var Backbone = require('backbone');
 
 module.exports = Backbone.Model.extend({
-  urlRoot: 'centres.json'
+  urlRoot: 'centres.json',
+  defaults: {
+    "bedcount": 0
+  },
+
+  initialize: function(attributes, options) {
+    this.socket = null;
+    var $this = this;
+
+    if (options && options.socket) this.socket = options.socket;
+
+    if (this.socket) {
+      this.socket.on('bedcount', function(payload) {
+        $this.set('bedcount', payload.beds);
+      });
+    }
+
+  }
 });
