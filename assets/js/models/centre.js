8306a8e3b5bc7ab82b282d8a253bd8ce08af159e
Stephen Fulljames <stephen@fulljames.net>
2015-10-16 11:46:58 +0100

    Tests for overal centre capacity and male and female availability

--- assets/js/models/centre.js
+++ assets/js/models/centre.js
@@ -1,4 +1,5 @@
 var Backbone = require('backbone');
+var _ = require('underscore');
 
 module.exports = Backbone.Model.extend({
   urlRoot: 'centres.json',
@@ -15,5 +16,27 @@ module.exports = Backbone.Model.extend({
       });
     }
 
+    this.on('change:beds', this.calculateCapacity, this);
+  },
+
+  calculateCapacity: function() {
+    var beds = this.get('beds');
+
+    var male_beds = _.find(beds, function(item) {
+      return item.type === 'male';
+    });
+
+    var female_beds = _.find(beds, function(item) {
+      return item.type === 'female';
+    });
+
+
+    this.set('capacity', _.reduce(beds, function(memo, item){
+      return memo + item.capacity;
+    }, 0));
+
+    if (male_beds) this.set('male_available', male_beds.capacity - (male_beds.booked + male_beds.prebooked + male_beds.ooc));
+    if (female_beds) this.set('female_available', female_beds.capacity - (female_beds.booked + female_beds.prebooked + female_beds.ooc));
   }
+
 });
