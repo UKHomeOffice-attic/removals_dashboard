a7ac5da09bf83293b08c0ee077cef35c3873e932
Stephen Fulljames <stephen@fulljames.net>
2015-10-23 10:16:27 +0100

    Extend faker to generate more meaningful simulated JSON

--- assets/schema/generate.js
+++ assets/schema/generate.js
@@ -1,29 +1,31 @@
 var tv4 = require('tv4');
-var json_schema_faker = require('json-schema-faker');
 var _ = require('underscore');
 
-var args = process.argv.slice(2);
+var schema = require('../schema/');
 
-if (args.length !== 2) {
-  throw new Error("Invalid arguments should be node generate.js [file path] [number of of fakes to make]");
-}
+var centres = [null,"First","Second","Third","Fourth","Fifth","Sixth","Seventh","Eighth","Ninth","Tenth","Eleventh","Twelveth"];
 
-var schema = require('./' + args[0]);
+var generate = function(schema, idx) {
+  var json_schema_faker = require('json-schema-faker');
+  var idx = idx || _.random(1,3);
 
-var number_of_fakes = args[1];
+  json_schema_faker.extend('faker', function(faker) {
+    faker.custom = {
+      hateoasUrl: function() {
+        var verb = _.sample(["","events","bookings","prebookings"]);
+        return faker.internet.domainWord() + '/' + idx + '/' + verb;
+      },
+      centreId: function() {
+        return idx;
+      },
+      centreName: function() {
+        return centres[idx];
+      }
+    };
 
-json_schema_faker.extend('faker', function(faker) {
-  faker.custom = {
-    hateoasUrl: function() {
-      var verb = _.sample(["","events","bookings","prebookings"]);
-      return faker.internet.domainWord() + '/' + _.random(1,12) + '/' + verb;
-    }
-  };
+    return faker;
+  });
 
-  return faker;
-});
-
-var fakes = _.map(_.range(number_of_fakes), function () {
   var faked_json = json_schema_faker(schema);
 
   // Be sure that we've got a valid bit of fake json
@@ -32,6 +34,11 @@ var fakes = _.map(_.range(number_of_fakes), function () {
     throw new Error(validation_response.error);
   }
   return faked_json;
-});
 
-process.stdout.write(JSON.stringify(fakes, null, " ") + '\n');
+};
+
+module.exports = {
+  Centre: function(idx) {
+    return generate(schema.centre, idx);
+  }
+};
