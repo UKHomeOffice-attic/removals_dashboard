8c1fcd44c07a2528d2c9efbf63d764c8964e99db
Chris Nesbitt-Smith <chris@cns.me.uk>
2015-11-11 13:10:31 +0000

    Refactor duplicated socket code in app router

--- assets/js/app.js
+++ assets/js/app.js
@@ -15,91 +15,75 @@ var staticParam = _.find(params, function(item) {
   return item.match('static');
 });
 
-var myRouter = Backbone.Router.extend({
+var DashboardRouter = Backbone.Router.extend({
+  routes: {
+    "": "handleRouteAvailability",
+    "?simulator": "handleRouteAvailability",
+    "?simulator&static": "handleRouteAvailability",
+    "availability": "handleRouteAvailability",
+    "statistics": "handleRouteStat"
+  },
 
-    routes: {
-      "": "handleRouteAvailability",
-      "?simulator": "handleRouteAvailability",
-      "?simulator&static": "handleRouteAvailability",
-      "availability": "handleRouteAvailability",
-      "statistics": "handleRouteStat"
-    },
+  initialize: function() {
+    console.log('router init');
+    var io;
 
-    handleRouteAvailability: function () {
-      var io;
-      var socket0;
-      var container = $('#content_container');
-
-      if (simulatorParam) {
-        var simulator = require('./socketSimulator');
-        socketio = simulator.client;
-        if (!staticParam) {
-          simulator.start();
-        }
+    if (simulatorParam) {
+      var simulator = require('./socketSimulator');
+      socketio = simulator.client;
+      if (!staticParam) {
+        simulator.start();
       }
-
-      io = sailsIOClient(socketio);
-
-      io.sails.autoConnect = false;
-      if (!simulatorParam) io.sails.url = 'http://localhost:8080';
-
-      socket0 = io.sails.connect();
-
-      container.empty();
-
-      socket0.get('/centre', function serverResponded(payload) {
-        _.each(payload.data, function(item,idx) {
-          container.append('<div class="centre_data" id="item'+idx+'"></div>');
-          var thisViewCentre = new viewCentre({
-            el: '#item'+idx,
-            socket: socket0
-          });
-          thisViewCentre.model.set(item);
-        })
-      });
-      $(".centre_data").wrapAll("<div id='centres' />");
-      $("<h1>IRC bed availability</h1>").insertBefore("#item0");
-    },
-
-    handleRouteStat: function () {
-      var io;
-      var socket1;
-      var container = $('#content_container');
-
-      if (simulatorParam) {
-        var simulator = require('./socketSimulator');
-        socketio = simulator.client;
-        if (!staticParam) {
-          simulator.start();
-        }
-      }
-
-      io = sailsIOClient(socketio);
-
-      io.sails.autoConnect = false;
-      if (!simulatorParam) io.sails.url = 'http://localhost:8080';
-
-      socket1 = io.sails.connect();
-
-      container.empty();
-
-      socket1.get('/centre', function serverResponded(payload) {
-        _.each(payload.data, function(item,idx) {
-          container.append('<div class="stat_data" id="stat'+idx+'"></div>');
-          var thisViewStat = new viewStat({
-            el: '#stat'+idx,
-            socket: socket1
-          });
-          thisViewStat.model.set(item);
-        })
-      });
-      $(".stat_data").wrapAll("<div id='stats' />");
-      $("<h1>IRC bed stats</h1>").insertBefore("#stat0");
     }
 
+    io = sailsIOClient(socketio);
+
+    io.sails.autoConnect = false;
+
+    if (!simulatorParam) io.sails.url = 'http://localhost:8080';
+
+    this.container = $('#content_container');
+    this.socket0 = io.sails.connect();
+  },
+
+  handleRouteAvailability: function() {
+    var self = this;
+
+    self.container.empty();
+    self.container.append("<h1>IRC bed availability</h1>");
+
+    self.socket0.get('/centre', function serverResponded(payload) {
+      _.each(payload.data, function(item,idx) {
+        self.container.append('<div class="centre_data" id="item'+idx+'"></div>');
+        var thisViewCentre = new viewCentre({
+          el: '#item'+idx,
+          socket: self.socket0
+        });
+        thisViewCentre.model.set(item);
+      })
+    });
+  },
+
+  handleRouteStat: function() {
+    var self = this;
+
+    self.container.empty();
+    self.container.append("<h1>IRC bed stats</h1>");
+
+    self.socket0.get('/centre', function serverResponded(payload) {
+      _.each(payload.data, function(item,idx) {
+        self.container.append('<div class="stat_data" id="stat'+idx+'"></div>');
+        var thisViewStat = new viewStat({
+          el: '#stat'+idx,
+          socket: self.socket0
+        });
+        thisViewStat.model.set(item);
+      })
+    });
+  }
 });
 
 $(function () {
-  router = new myRouter();
+  new DashboardRouter();
   Backbone.history.start();
 });
