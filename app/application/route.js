d877ecd04ece7cd79e9dd09f92f85c17abe99c20
Chelsea Sanders <cksanders1@gmail.com>
2016-01-25 09:39:49 +0000

    BM-416 Inject and activate sails-io in application route

--- app/application/route.js
+++ app/application/route.js
@@ -3,5 +3,12 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model() {
     return this.store.findAll('centre');
+  },
+  activate() {
+    this.io.socket.get('/centres', function serverResponded(body, JWR) {
+      console.log('Sails responded with: ', body);
+      console.log('with headers: ', JWR.headers);
+      console.log('and with status code: ', JWR.statusCode);
+    });
   }
 });
