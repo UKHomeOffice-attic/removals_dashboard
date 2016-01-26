f4da194751998f20cdbfce95713c8712299b5bf1
Chelsea Sanders <cksanders1@gmail.com>
2016-01-26 00:10:14 +0000

    BM-417 Application Route to update datastore on server messages received via socket-io

--- app/application/route.js
+++ app/application/route.js
@@ -1,14 +1,26 @@
 import Ember from 'ember';
+import DS from 'ember-data';
 
 export default Ember.Route.extend({
+  beforeModel() {
+    var self = this;
+    self.io.socket.on('centres', function(message) {
+      var data = message.data;
+      var modelClass = self.store.modelFor(data.type);
+      var serializer = self.store.serializerFor('application');
+      var json = serializer.normalizeSingleResponse(self.store, modelClass, data, data.id);
+
+      self.store.push(json);
+    });
+  },
   model() {
     return this.store.findAll('centre');
   },
   activate() {
     this.io.socket.get('/centres', function serverResponded(body, JWR) {
-      console.log('Sails responded with: ', body);
-      console.log('with headers: ', JWR.headers);
-      console.log('and with status code: ', JWR.statusCode);
     });
+  },
+  deactivate() {
+    this.self.io.socket.disconnect();
   }
 });
