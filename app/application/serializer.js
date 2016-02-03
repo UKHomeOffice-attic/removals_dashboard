99f1d756f4165fa95a872b5c3b4164a6eb9b2284
Chelsea Sanders <cksanders1@gmail.com>
2016-02-03 13:42:50 +0000

    BM-364 Remove redundant custom functionality previously extending serializer

--- app/application/serializer.js
+++ app/application/serializer.js
@@ -1,20 +1,9 @@
 /**
  * @file RESTSerializer implementation.
- * Ember CLI Mirage 0.1.x exposes a REST API and Ember Data should be aware. Furthermore, this implements a custom
- * Serializer by extending keyForAttribute and keyForRelationship. This does not match the JSONAPISerializer that
- * ships with Ember Data (matching the examples on http://jsonapi.org/format).
+ * The JSONAPISerializer matches the JSONAPISerializer that ships with Ember Data (matching the examples on
+ * http://jsonapi.org/format). Ember CLI Mirage 0.1.x exposes a REST API and Ember Data should be aware, regardless of
+ * the Serilizer having extended functionality or not.
  */
-import Ember from 'ember';
 import DS from 'ember-data';
 
-export default DS.JSONAPISerializer.extend({
-  camelize: Ember.String.camelize,
-
-  keyForAttribute: function(attr) {
-    return this.camelize(attr);
-  },
-
-  keyForRelationship: function(rawKey) {
-    return this.camelize(rawKey);
-  }
-});
+export default DS.JSONAPISerializer.extend({});
