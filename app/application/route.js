10d30c90311352e325501ff50f6118d6c1321994
Chelsea Sanders <cksanders1@gmail.com>
2016-02-05 14:48:33 +0000

    BM-455: Update payload to use model specific serializer

--- app/application/route.js
+++ app/application/route.js
@@ -7,7 +7,7 @@ export default Ember.Route.extend({
     self.io.socket.get('/centres', () =>
       self.io.socket.on('centres', message => {
         let modelClass = self.store.modelFor(message.data.type);
-        let serializer = self.store.serializerFor('application');
+        let serializer = self.store.serializerFor(message.data.type);
         var json = serializer.extractSingle(self.store, modelClass, message.data, message.data.id);
 
         self.store.push(json);
