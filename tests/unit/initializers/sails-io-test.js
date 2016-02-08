e8dd26d8230b1f2b662dc3db894dc88d397a88ac
Chelsea Sanders <cksanders1@gmail.com>
2016-02-08 17:38:09 +0000

    BM-364 Update SailsIO Initializer Unit test

--- tests/unit/initializers/sails-io-test.js
+++ tests/unit/initializers/sails-io-test.js
@@ -1,22 +1,21 @@
 import Ember from 'ember';
 import SailsIoInitializer from '../../../initializers/sails-io';
-import { module, skip } from 'qunit';
+import { module, test } from 'qunit';
 
-let application;
+let app;
+let container;
 
 module('Unit | Initializer | sails io', {
   beforeEach() {
     Ember.run(function() {
-      application = Ember.Application.create();
-      application.deferReadiness();
+      app = Ember.Application.create();
+      app.deferReadiness();
     });
   }
 });
 
-// Temporarily skipped until BM-364 is done.
-skip('it works', function(assert) {
-  SailsIoInitializer.initialize(application);
+test('it works', function(assert) {
+  SailsIoInitializer.initialize(container, app);
 
-  // you would normally confirm the results of the initializer here
   assert.ok(true);
 });
